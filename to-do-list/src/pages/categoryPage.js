7db7ea62e465108997b3fc7797be63ef00ed06ea
angelaoryza <angelaoryza@gmail.com>
2023-05-22 23:06:18 +0700

    Cryig

--- to-do-list/src/pages/categoryPage.js
+++ to-do-list/src/pages/categoryPage.js
@@ -36,7 +36,7 @@ const CategoryTask = () => {
         };
         handleFilter()
 
-        }, [tasks, {cat}])
+        }, [cat])
     const handleOnChange = async (id, done) => {
     
         const taskDocRef = doc(db, 'to-do-list', id)
